a459beed8ee0e85faa751e8ce613b81bf087117c
Julian T <julian.thiel@outlook.com>
2024-03-27 18:00:38 +0100

    Some refactoring of deprecated syntax

--- js/controllers/EmailController.js
+++ js/controllers/EmailController.js
@@ -1,5 +1,7 @@
 app.controller('EmailController', ['$scope', 'emails', '$routeParams', function ($scope, emails, $routeParams) {
-    emails.success(function (data) {
+    emails.then(function (data) {
         $scope.email = data[$routeParams.id];
+    }).catch(function (error) {
+        console.error('Error fetching emails:', error);
     });
 }]);
--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,5 +1,7 @@
 app.controller('HomeController', ['$scope', 'emails', function ($scope, emails) {
-    emails.success(function (data) {
+    emails.then(function (data) {
         $scope.emails = data;
+    }).catch(function (error) {
+        console.error('Error fetching emails:', error);
     });
 }]);
