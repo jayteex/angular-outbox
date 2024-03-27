5772c6cf0ec48690c8beb34fffa15f7ee495832b
Julian T <julian.thiel@outlook.com>
2024-03-27 17:56:18 +0100

    Minor changes

--- js/services/emails.js
+++ js/services/emails.js
@@ -1,9 +1,11 @@
 app.factory('emails', ['$http', function ($http) {
     return $http.get('https://content.codecademy.com/courses/ltp4/emails-api/emails.json')
-        .success(function (data) {
-            return data;
+        .then(function (response) {
+            return response.data;
         })
-        .error(function (err) {
-            return err;
+        .catch(function (error) {
+            // Handle error
+            console.error('Error fetching emails:', error);
+            throw error; // Rethrow the error to propagate it to the caller
         });
 }]);
