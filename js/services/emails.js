fa7eb16616b4ebb3950ef31952407b69f9097b70
Julian T <julian.thiel@outlook.com>
2024-03-27 18:29:51 +0100

    Minor changes

--- js/services/emails.js
+++ js/services/emails.js
@@ -4,8 +4,7 @@ app.factory('emails', ['$http', function ($http) {
             return response.data;
         })
         .catch(function (error) {
-            // Handle error
             console.error('Error fetching emails:', error);
-            throw error; // Rethrow the error to propagate it to the caller
+            throw error; 
         });
 }]);
